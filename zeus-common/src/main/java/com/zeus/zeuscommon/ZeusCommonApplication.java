9135ceea37d43a44b2dde06eb69c5fda0a82b957
wangyz <wangyuzhong@cmdi.chinamobile.com>
2021-01-26 14:42:42 +0800

    Initial commit

---
+++ zeus-common/src/main/java/com/zeus/zeuscommon/ZeusCommonApplication.java
@@ -0,0 +1,13 @@
+package com.zeus.zeuscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeusCommonApplication.class, args);
+    }
+
+}
