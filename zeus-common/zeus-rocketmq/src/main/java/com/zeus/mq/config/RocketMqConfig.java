1377b70dc284e3af9ab597cda5f2932ed7af407f
wangyz <wangyuzhong@cmdi.chinamobile.com>
2021-02-01 17:00:18 +0800

    引入rocketmq 写starter

--- zeus-common/zeus-rocketmq/src/main/java/com/zeus/mq/config/RocketMqConfig.java
+++ zeus-common/zeus-rocketmq/src/main/java/com/zeus/mq/config/RocketMqConfig.java
@@ -1,19 +1,24 @@
 package com.zeus.mq.config;
 
+import com.zeus.rocketmq.configuration.RocketMqProperties;
 import com.zeus.rocketmq.core.producer.ProducerConfig;
 import com.zeus.rocketmq.core.producer.RocketMqProducerTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RocketMqConfig {
 
+    @Autowired
+    private RocketMqProperties rocketMqProperties;
+
     @Bean
     public RocketMqProducerTemplate getProduct(){
         RocketMqProducerTemplate rocketMqProducerTemplate = new RocketMqProducerTemplate();
         ProducerConfig producerConfig = new ProducerConfig();
         producerConfig.setProducerGroup("DEFAULT_GROUP");
-        rocketMqProducerTemplate.setNamesrvAddr("192.168.1.121:9876");
+        rocketMqProducerTemplate.setNamesrvAddr(rocketMqProperties.getNameSrvAddr());
         rocketMqProducerTemplate.setProducerConfig(producerConfig);
         return rocketMqProducerTemplate;
     }
