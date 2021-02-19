6a6e519c6c75c8c1221385f885a05d0b921611c7
wangyz <wangyuzhong@cmdi.chinamobile.com>
2021-02-19 14:08:01 +0800

    使用责任链模式编写参数校验demo

--- zeus-common/zeus-commons-starter/src/main/java/com/zeus/commons/configuration/CommonsAutoConfiguration.java
+++ zeus-common/zeus-commons-starter/src/main/java/com/zeus/commons/configuration/CommonsAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.zeus.commons.configuration;
 import com.zeus.core.handle.VerifyHandler;
 import com.zeus.core.handle.VerifyHandlerChain;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +12,15 @@ import org.springframework.context.annotation.Bean;
 @EnableConfigurationProperties(CommonProperties.class)
 public class CommonsAutoConfiguration {
 
+    @Autowired
+    private CommonProperties commonProperties;
+
     @Bean
     @ConditionalOnBean(VerifyHandler.class)
     public VerifyHandlerChain verifyHandlerChain() {
         log.info("开始创建verifyHandlerChain=====================");
         VerifyHandlerChain verifyHandlerChain = new VerifyHandlerChain();
+        log.info(commonProperties.getEnableVerify() + "");
         return verifyHandlerChain;
     }
 }
